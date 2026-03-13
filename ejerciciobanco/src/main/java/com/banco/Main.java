6ca5d19fbdda4cab84559f6e3be12dcc739f5fc3
Juan D Ortiz <jdavorgor@gmail.com>
2026-03-13 16:52:43 -0500

    Implementacion clase main para probar las cuentas

--- ejerciciobanco/src/main/java/com/banco/Main.java
+++ ejerciciobanco/src/main/java/com/banco/Main.java
@@ -1,7 +1,28 @@
 package com.banco;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("=== CUENTA DE AHORROS ===");
+        CuentaAhorros cuentaAhorros = new CuentaAhorros(15000, 5);
+
+        cuentaAhorros.consignar(2000);
+        cuentaAhorros.retirar(3000);
+        cuentaAhorros.retirar(1000);
+        cuentaAhorros.retirar(500);
+        cuentaAhorros.retirar(200);
+        cuentaAhorros.retirar(100);
+
+        cuentaAhorros.extractoMensual();
+        cuentaAhorros.imprimir();
+
+        System.out.println("\n=== CUENTA CORRIENTE ===");
+        CuentaCorriente cuentaCorriente = new CuentaCorriente(10000, 5);
+
+        cuentaCorriente.retirar(12000);
+        cuentaCorriente.consignar(3000);
+        cuentaCorriente.extractoMensual();
+        cuentaCorriente.imprimir();
     }
 }
